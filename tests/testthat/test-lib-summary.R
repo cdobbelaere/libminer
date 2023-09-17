2516a16f7eddb351778fb9fb3473f4812f2bbeb5
cdobbelaere <cristinadobbelaere@gmail.com>
2023-09-17 16:14:23 -0500

    Update tests for new `sizes` argument

--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -13,9 +13,17 @@ test_that("lib_summary returns expected result", {
 })
 
 test_that("lib_summary fails appropriately", {
-  # if someone calls `lib_summary()` with an argument,
-  # we're telling it to throw an error ("unused argument").
-  # you can use regex here. if you expect your error message to vary more,
-  # you can eliminate the last argument here to be more general.
-  expect_error(lib_summary("foo"), "unused argument")
+  # if someone calls `lib_summary()` with a non-logical argument,
+  # we're telling it to throw an error.
+  expect_error(lib_summary("foo"), "'sizes' must be TRUE or FALSE.")
+})
+
+test_that("sizes argument works", {
+  res <- lib_summary(sizes = TRUE)
+  # expect 3 columns now
+  expect_equal(ncol(res), 3)
+  # expect a third column to be added when `sizes = TRUE`
+  expect_equal(names(res), c("Library", "n_packages", "lib_size"))
+  # expect `lib_size` column to be type double
+  expect_type(res$lib_size, "double")
 })
