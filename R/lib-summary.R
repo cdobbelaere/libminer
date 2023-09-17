5cc51fb90bf3846e82c3876b624edc4e5e737934
cdobbelaere <cristinadobbelaere@gmail.com>
2023-09-17 11:54:05 -0500

    Add MIT license update

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,7 +1,11 @@
 lib_summary <- function() {
   # function to tell us how many pkgs in each library
-  pkgs <- utils::installed.packages() # gives character matrix of all packages installed
+
+  # get character matrix of all packages installed
+  pkgs <- utils::installed.packages()
+
   pkg_tbl <- table(pkgs[, "LibPath"])
+
   # turn table into df
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
